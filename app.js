0aa699c8508ceb3814459f431dcdd167ffca8b2e
Student <LAB-205@aptech-msg.edu>
2023-11-30 10:55:21 +0500

    js modified

--- app.js
+++ app.js
@@ -1,21 +1,26 @@
+var get_value = document.getElementById("search").value;
+
 function handleclick() {
-    let get_value = document.getElementById("search").value;
+   get_value = document.getElementById("search").value;
+    document.getElementById("search").value="";
     document.getElementById("list").innerHTML += `
 <div class="list_container">
             <li>${get_value}</li>
-            <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick=modal()>Edit</button>
+            <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick=modal(this)>Edit</button>
             <button>Delete</button>
         </div>
 `
 }
 
 // modal function
-
-function modal() {
+  
+function modal(abc) {
+get_child=abc.parenteElement.children[0]
+get_edit=abc.parenteElement.children[0].innerHTML;
      document.querySelector(".modal-dialog").innerHTML= `
     <div class="modal-content">
     <div class="modal-body">
-      <input type="search">
+      <input type="search" value=${get_value}>
         <button>Update</button>
     </div>
     <div class="modal-footer">
