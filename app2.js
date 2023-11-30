14849f4d86b3b6d9d32fa610e43c1fffc5bef923
Hassanhnu9 <147471879+Hassanhnu9@users.noreply.github.com>
2023-11-30 08:25:29 +0500

    task completed

---
+++ app2.js
@@ -0,0 +1,31 @@
+function handleclick() {
+    let get_value = document.getElementById("search").value;
+    document.getElementById("list").innerHTML += `
+<div class="list_container">
+            <li>${get_value}</li>
+            <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick=modal()>Edit</button>
+            <button>Delete</button>
+        </div>
+`
+}
+
+
+
+
+
+// modal function
+
+function modal() {
+    document.querySelector(".modal-dialog").innerHTML= `
+   <div class="modal-content">
+   <div class="modal-body">
+     <input type="search">
+       <button>Update</button>
+   </div>
+   <div class="modal-footer">
+     <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+     <button type="button" class="btn btn-primary">Save changes</button>
+   </div>
+ </div>
+   `
+}
