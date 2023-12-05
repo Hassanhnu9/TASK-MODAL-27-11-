7f53baac6e69e6eb91baefc04dd2b4399efd3439
Student <LAB-205@aptech-msg.edu>
2023-12-05 10:55:04 +0500

    modified(5-12)

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 var get_value = document.getElementById("search").value;
-
+let arr = [];
 function handleclick() {
-   get_value = document.getElementById("search").value;
-    document.getElementById("search").value="";
-    document.getElementById("list").innerHTML += `
+  get_value = document.getElementById("search").value;
+  arr.push(get_value)
+  localStorage.setItem("list", arr);
+  console.log(arr)
+
+  document.getElementById("search").value = "";
+  document.getElementById("list").innerHTML += `
 <div class="list_container">
             <li>${get_value}</li>
             <button data-bs-toggle="modal" data-bs-target="#exampleModal" onclick=modal(this)>Edit</button>
-            <button>Delete</button>
+            <button onclick=deleteData(this)>Delete</button>
         </div>
 `
 }
 
 // modal function
-  
+
+var get_child;
+var get_edit;
+
 function modal(abc) {
-get_child=abc.parenteElement.children[0]
-get_edit=abc.parenteElement.children[0].innerHTML;
-     document.querySelector(".modal-dialog").innerHTML= `
+  get_child = abc.parenteElement.children[0]
+  get_edit = abc.parenteElement.children[0].innerHTML;
+  document.querySelector(".modal-body").innerHTML = `
     <div class="modal-content">
     <div class="modal-body">
-      <input type="search" value=${get_value}>
-        <button>Update</button>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-      <button type="button" class="btn btn-primary">Save changes</button>
+      <input type="search" value=${get_edit} id="changeInput">
+        <button onclick=addData(this)>Update</button>
     </div>
   </div>
     `
+}
+
+// add data
+function addData(a) {
+  let storeVal = document.getElementById("changeInput").value
+  getChild.innerHTML = storeVal
+
+}
+
+// delete data
+function deleteData(getObject) {
+  getObject.parentElement.remove()
+
 }
